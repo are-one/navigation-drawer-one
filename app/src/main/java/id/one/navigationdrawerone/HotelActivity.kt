dcd64a2b9b03d0680536fbe7c911fb443d9ea8d4
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-08-05 19:21:04 +0800

    [UPDATE] Tambah activity menu navigasi drawer

---
+++ app/src/main/java/id/one/navigationdrawerone/HotelActivity.kt
@@ -0,0 +1,11 @@
+package id.one.navigationdrawerone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel)
+    }
+}
